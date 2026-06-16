4e67e9f57beacee453cc551bc8942834b8819e97
IM4FE <dima.selivanov1020@mail.ru>
2026-06-16 18:31:18 +0300

    initial commit:just addeed new item

---
+++ src/client/java/org/sgx/bamboo_grenade/client/BambooGrenadeClient.java
@@ -0,0 +1,10 @@
+package org.sgx.bamboo_grenade.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BambooGrenadeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/org/sgx/bamboo_grenade/item/ModItems.java
@@ -0,0 +1,28 @@
+package org.sgx.bamboo_grenade.item;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import org.sgx.bamboo_grenade.BambooGrenade;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item BAMBOO_GRENADE = registerItem("bamboo_grenade", Item::new);
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(BambooGrenade.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(BambooGrenade.MOD_ID, name)))));
+    }
+
+    public static void register(){
+        BambooGrenade.LOGGER.debug("Registering items for:" + BambooGrenade.MOD_ID);
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.COMBAT).register(output -> output.accept(BAMBOO_GRENADE));
+    }
+}
