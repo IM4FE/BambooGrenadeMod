a618b29fbaa2656c0f0925732334b9a8d9c4717b
IM4FE <dima.selivanov1020@mail.ru>
2026-06-17 19:21:56 +0300

    change onHit event to seeding random count bamboo in random place in area 8x8

--- src/main/java/org/sgx/bamboo_grenade_mod/entity/BambooGrenadeEntity.java
+++ src/main/java/org/sgx/bamboo_grenade_mod/entity/BambooGrenadeEntity.java
@@ -1,14 +1,22 @@
 package org.sgx.bamboo_grenade_mod.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.phys.HitResult;
 import org.sgx.bamboo_grenade_mod.init.ModEntities;
 import org.sgx.bamboo_grenade_mod.init.ModItems;
+
+import java.util.Vector;
 
 public class BambooGrenadeEntity extends ThrowableItemProjectile {
     public BambooGrenadeEntity(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {
@@ -23,7 +31,20 @@ public class BambooGrenadeEntity extends ThrowableItemProjectile {
     protected void onHit(HitResult hitResult) {
         super.onHit(hitResult);
         if (!this.level().isClientSide()) {
-            this.level().explode(this, this.getX(), this.getY(), this.getZ(), 3.0f, Level.ExplosionInteraction.TNT);
+            //BlockPos.betweenClosedStream(this.blockPosition(), this.blockPosition().north(3)).forEach(pos->{
+            RandomSource random = this.level().getRandom();
+            for (int i = 0; i <= random.nextInt(4, 16); i++){
+                int offsetX = random.nextInt(-4, 4);
+                int offsetZ = random.nextInt(-4,4);
+
+                BlockPos targetPos = this.blockPosition().offset(offsetX, 0, offsetZ);
+                targetPos = targetPos.atY(this.level().getHeight(Heightmap.Types.MOTION_BLOCKING, targetPos.getX(), targetPos.getZ()));
+                if (!this.level().isOutsideBuildHeight(targetPos)) {
+                    this.level().setBlockAndUpdate(targetPos, Blocks.BAMBOO_SAPLING.defaultBlockState());
+                }
+            }
+            //});
+            //this.level().explode(this, this.getX(), this.getY(), this.getZ(), 100.0f, Level.ExplosionInteraction.TNT);
             this.discard();
         }
     }
